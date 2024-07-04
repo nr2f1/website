450d6a13d574e285b56fba993f4576bfb489f20b
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-04 20:37:10 +0100

    perf: build working

--- website-assets/vite.config.ts
+++ website-assets/vite.config.ts
@@ -6,6 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../node_modules/.vite/website-assets',
+  base: './',
 
   server: {
     port: 4200,
@@ -22,6 +23,7 @@ export default defineConfig({
   build: {
     outDir: '../dist/website-assets',
     reportCompressedSize: true,
+    assetsDir: './',
     commonjsOptions: {
       transformMixedEsModules: true,
     },
