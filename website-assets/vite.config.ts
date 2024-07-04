a2e1e45d6104b71036ef49e5b79bce9c31c01a4f
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-04 19:56:57 +0100

    ci: draft a workflow to deploy assets into github pages

--- website-assets/vite.config.ts
+++ website-assets/vite.config.ts
@@ -19,11 +19,6 @@ export default defineConfig({
 
   plugins: [nxViteTsPaths()],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   build: {
     outDir: '../dist/website-assets',
     reportCompressedSize: true,
