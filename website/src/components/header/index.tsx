6b991c92c7126951725b3cd445400859e08c7c6d
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-07 15:35:53 +0100

    style: top header

--- website/src/components/header/index.tsx
+++ website/src/components/header/index.tsx
@@ -9,32 +9,123 @@ import LocaleSelector from './locale-selector';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.header__logo}>
-        <Link href="/">
-          <Image
-            src={`${ASSETS_URL}/nr2f1-foundation-logo-color-white-text-original.png`}
-            alt="nr2f1 foundation logo"
-            width={1000}
-            height={1000}
-            loading="eager"
-          />
-        </Link>
-      </div>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/" className="button button--on-dark">
-                Register a patient
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="button button--accent-on-dark">
-                Donate
-              </Link>
-            </li>
-          </ul>
+      <div className={styles.header__top}>
+        <div className={styles.header__logo}>
+          <Link href="/">
+            <Image
+              src={`${ASSETS_URL}/nr2f1-foundation-logo-color-white-text-original.png`}
+              alt="nr2f1 foundation logo"
+              width={1000}
+              height={1000}
+              loading="eager"
+            />
+          </Link>
+        </div>
+        <div className={styles.header__top_right}>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" className="button button--on-dark">
+                  Register a patient
+                </Link>
+              </li>
+              <li>
+                <Link href="/" className="button button--accent-on-dark">
+                  Donate
+                </Link>
+              </li>
+            </ul>
+          </nav>
           <LocaleSelector />
+        </div>
+      </div>
+      <div className={styles.header__bottom}>
+        <nav>
+          <details>
+            <summary>About BBSOS</summary>
+            <ul>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary>Living with BBSOS</summary>
+            <ul>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary>Research</summary>
+            <ul>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary>About us</summary>
+            <ul>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary>Support us</summary>
+            <ul>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+              <li>
+                <Link href="/">Menu Item</Link>
+              </li>
+            </ul>
+          </details>
         </nav>
       </div>
     </header>
