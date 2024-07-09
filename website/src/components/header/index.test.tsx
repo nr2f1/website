830540e9b97594b980f5e89503fd47a108820695
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-09 19:05:09 +0100

    fix: locale selector z index

--- website/src/components/header/index.test.tsx
+++ website/src/components/header/index.test.tsx
@@ -8,7 +8,6 @@ vi.mock('next/navigation', async (importOriginal) => {
   return {
     // @ts-ignore
     ...actual,
-    // your mocked methods
     useRouter() {
       return {
         push: () => vi.fn(),
@@ -20,16 +19,32 @@ vi.mock('next/navigation', async (importOriginal) => {
   };
 });
 
+vi.mock('next/image', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    // @ts-ignore
+    ...actual,
+    default(props: unknown) {
+      // @ts-ignore
+      // biome-ignore lint/a11y/useAltText: This is a test file
+      return <img {...props} />;
+    },
+  };
+});
+
 describe('Header', () => {
-  it('should render', async () => {
+  it('renders without any accessibility violation', async () => {
     const { container } = render(<Header />);
     const results = await waitFor(() => axe(container), { timeout: 5000 });
     expect(results).toHaveNoViolations();
   });
 
-  it('should render', async () => {
+  it('render', async () => {
     const { getByRole } = render(<Header />);
-    const logo = getByRole('img', { name: 'nr2f1 foundation logo' });
-    expect(logo).toBeInTheDocument();
+
+    await waitFor(() => {
+      const logo = getByRole('img', { name: 'nr2f1 foundation logo' });
+      expect(logo).toBeInTheDocument();
+    });
   });
 });
--- website/src/components/header/index.tsx
+++ website/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.scss';
 
 import { ASSETS_URL } from '@config/utils';
-// import Image from 'next/image';
+import Image from 'next/image';
 import Link from 'next/link';
 import LocaleSelector from './locale-selector';
 import NavList, { NavItem } from './nav-list';
@@ -31,20 +31,13 @@ const Header = () => {
       <div className={styles.header__top}>
         <div className={styles.header__logo}>
           <Link href="/">
-            {/* <Image
+            <Image
               src={`${ASSETS_URL}/nr2f1-foundation-logo-color-white-text-original.png`}
               alt="nr2f1 foundation logo"
               width={1000}
               height={1000}
               loading="eager"
-              // priority={true}
-            /> */}
-            <picture>
-              <img
-                src={`${ASSETS_URL}/nr2f1-foundation-logo-color-white-text-original.png`}
-                alt="nr2f1 foundation logo"
-              />
-            </picture>
+            />
           </Link>
         </div>
         <div className={styles.header__top_right}>
