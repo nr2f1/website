ff0ee45533f6b0773bb818b947bf288241082c57
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-27 12:48:24 +0100

    style: set content wrapper around header

--- website/src/components/header/index.tsx
+++ website/src/components/header/index.tsx
@@ -58,49 +58,53 @@ const Header = () => {
       }`}
     >
       <div className={styles.header__top}>
-        <div className={styles.header__logo}>
-          <Link href="/">
-            <MainLogo />
-          </Link>
+        <div className={styles.header__content_wrapper}>
+          <div className={styles.header__logo}>
+            <Link href="/">
+              <MainLogo />
+            </Link>
+          </div>
+          <div className={styles.header__top_right}>
+            <nav title="primary">
+              <ul>
+                <li>
+                  <RegisterPatientButton />
+                </li>
+                <li>
+                  <DonateButton />
+                </li>
+              </ul>
+            </nav>
+            <LocaleSelector />
+            <button
+              title="hambuguer-button"
+              type="button"
+              className="button button--on-dark"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
+          </div>
         </div>
-        <div className={styles.header__top_right}>
-          <nav title="primary">
+      </div>
+      <div className={styles.header__bottom}>
+        <div className={styles.header__content_wrapper}>
+          <nav title="secondary">
+            <NavList name="About BBSOS" items={navItems} />
+            <NavList name="Living with BBSOS" items={navItems} />
+            <NavList name="Research" items={navItems} />
+            <NavList name="About us" items={navItems} />
+            <NavList name="Support us" items={navItems} />
+          </nav>
+          <div className={styles.header__bottom_medium_screen}>
             <ul>
               <li>
                 <RegisterPatientButton />
               </li>
               <li>
-                <DonateButton />
+                <DonateButton isMobile />
               </li>
             </ul>
-          </nav>
-          <LocaleSelector />
-          <button
-            title="hambuguer-button"
-            type="button"
-            className="button button--on-dark"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          />
-        </div>
-      </div>
-      <div className={styles.header__bottom}>
-        <nav title="secondary">
-          <NavList name="About BBSOS" items={navItems} />
-          <NavList name="Living with BBSOS" items={navItems} />
-          <NavList name="Research" items={navItems} />
-          <NavList name="About us" items={navItems} />
-          <NavList name="Support us" items={navItems} />
-        </nav>
-        <div className={styles.header__bottom_medium_screen}>
-          <ul>
-            <li>
-              <RegisterPatientButton />
-            </li>
-            <li>
-              <DonateButton isMobile />
-            </li>
-          </ul>
-          <LocaleSelector isMobile />
+            <LocaleSelector isMobile />
+          </div>
         </div>
       </div>
     </header>
