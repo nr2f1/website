cbb46f8ed2ba0f8640c5614409291fbbda7312ea
Pedro Martin <pedro.martin@generalassemb.ly>
2024-07-31 21:30:46 +0100

    refactor: getHeader query

---
+++ website/src/models/navlinks/index.ts
@@ -0,0 +1,5 @@
+export const aboutBbsoasId = '2UYFtEkEyzkPoTNsxNmFEb';
+export const livingWithBbsoasId = '4jbJTi8bRgkk7pYdKHXU9';
+export const researchId = '2TeHhRlvLTOomJiHaxNPUP';
+export const aboutUsId = '4Oi8Hm5OZH7kK8F6WgLOgg';
+export const supportUsId = 'Lw95NJKD8APWY3EJRdTkD';
