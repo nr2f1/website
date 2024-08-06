613246f1fe6cdcd884493ba289ed30da0292c8b4
Pedro Martin <pedro.martin@generalassemb.ly>
2024-08-06 09:25:28 +0100

    chore: markup all footer

--- website/src/components/footer/index.tsx
+++ website/src/components/footer/index.tsx
@@ -1,5 +1,8 @@
 import styles from './index.module.scss';
 
+import Contentful from '@components/logos/contentful';
+import MainLogo from '@components/logos/main';
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
@@ -79,6 +82,76 @@ const Footer = () => {
                 </button>
               </form>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer__middle}>
+        <div className={styles.footer__content_wrapper}>
+          <div className={styles.footer__middle_row}>
+            <div className={styles.footer__middle_column}>
+              <div className={styles.footer__logo}>
+                <MainLogo />
+              </div>
+              <p>
+                ©2021 NR2F1 Foundation. All Rights Reserved. Registered
+                501(c)(3) non-profit organization Federal ID# 83-2659721
+              </p>
+              <p>
+                This website is not designed to diagnose, advise or provide
+                medical opinions, treatment or services. It is for general
+                knowledge purposes only. Please consult a licensed medical
+                professional with all questions and concerns regarding a medical
+                condition.
+              </p>
+            </div>
+            <div className={styles.footer__middle_column}>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.facebook.com/BBSoas"
+                    className="button button--on-dark"
+                  >
+                    <span>YouTube</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/BBSoas"
+                    className="button button--on-dark"
+                  >
+                    <span>X</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/BBSoas"
+                    className="button button--on-dark"
+                  >
+                    <span>Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/BBSoas"
+                    className="button button--on-dark"
+                  >
+                    <span>Instagram</span>
+                  </a>
+                </li>
+              </ul>
+              <p>Contact us:</p>
+              <address>
+                NR2F1 Foundation 416 E. Kenilworth Ave Royal Oak, MI 48067
+                <a href="mailto:info@nr2f1.org">info@nr2f1.org</a>
+              </address>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer__bottom}>
+        <div className={styles.footer__content_wrapper}>
+          <div className={styles.footer__contentful}>
+            <Contentful />
           </div>
         </div>
       </div>
