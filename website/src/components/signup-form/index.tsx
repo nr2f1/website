a2d5671ac809b143e210a0300e81a8b38a14bc18
Pedro Martin <pedro.martin@generalassemb.ly>
2024-08-11 19:41:00 +0100

    feat: signup form validation

--- website/src/components/signup-form/index.tsx
+++ website/src/components/signup-form/index.tsx
@@ -7,7 +7,7 @@ import { Option } from '@mui/base/Option';
 import { Select } from '@mui/base/Select';
 import { useFormik } from 'formik';
 import { object, string } from 'yup';
-import { Role, roles, validatioErrorMessage } from './i18n';
+import { Role, englishContries, roles, validatioErrorMessage } from './i18n';
 
 interface SignupFormValues {
   firstname: string;
@@ -16,6 +16,10 @@ interface SignupFormValues {
   role: string | Role;
   patientFirstName?: string;
   country?: string;
+  region?: string;
+  streetAdress?: string;
+  city?: string;
+  postCode?: string;
 }
 
 const initialValues = {
@@ -25,6 +29,10 @@ const initialValues = {
   role: '',
   patientFirstName: '',
   country: '',
+  region: '',
+  streetAdress: '',
+  city: '',
+  postCode: '',
 };
 
 type MuiEvent =
@@ -41,6 +49,8 @@ const ErrorMessage: React.FC<ErrorMessageProps> = ({
   errorMessage = 'This is a mandatory field',
 }) => <p>{errorMessage}</p>;
 
+const allowedIsoCountryCodes = englishContries.map(({ value }) => value);
+
 const getValidationSchema = (lang: AvailableLocale) => {
   const errorMessage = validatioErrorMessage[lang];
 
@@ -55,6 +65,32 @@ const getValidationSchema = (lang: AvailableLocale) => {
       is: Role.ParentPatient,
       // biome-ignore lint/suspicious/noThenProperty: it is a valid method
       then: (schema) => schema.required(errorMessage),
+    }),
+    country: string().when('role', {
+      is: Role.ParentPatient,
+      // biome-ignore lint/suspicious/noThenProperty: it is a valid method
+      then: (schema) =>
+        schema.oneOf(allowedIsoCountryCodes).required(errorMessage),
+    }),
+    region: string().when('role', {
+      is: Role.ParentPatient,
+      // biome-ignore lint/suspicious/noThenProperty: it is a valid method
+      then: (schema) => schema.optional(),
+    }),
+    streetAdress: string().when('role', {
+      is: Role.ParentPatient,
+      // biome-ignore lint/suspicious/noThenProperty: it is a valid method
+      then: (schema) => schema.optional(),
+    }),
+    city: string().when('role', {
+      is: Role.ParentPatient,
+      // biome-ignore lint/suspicious/noThenProperty: it is a valid method
+      then: (schema) => schema.optional(),
+    }),
+    postCode: string().when('role', {
+      is: Role.ParentPatient,
+      // biome-ignore lint/suspicious/noThenProperty: it is a valid method
+      then: (schema) => schema.optional(),
     }),
   });
 };
@@ -85,9 +121,21 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
     validationSchema,
   });
 
-  const handleSelectOnChange = (_event: MuiEvent, newValue: string | null) => {
+  const handleSelectRoleOnChange = (
+    _event: MuiEvent,
+    newValue: string | null,
+  ) => {
     if (newValue) {
       setFieldValue('role', newValue);
+    }
+  };
+
+  const handleSelectCountryOnChange = (
+    _event: MuiEvent,
+    newValue: string | null,
+  ) => {
+    if (newValue) {
+      setFieldValue('country', newValue);
     }
   };
 
@@ -97,6 +145,11 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
   const roleError = touched.role && errors.role;
   const patientFirstNameError =
     touched.patientFirstName && errors.patientFirstName;
+  const countryError = touched.country && errors.country;
+  const regionError = touched.region && errors.region;
+  const cityError = touched.city && errors.city;
+  const streetAdressError = touched.streetAdress && errors.streetAdress;
+  const postCodeError = touched.postCode && errors.postCode;
 
   const showExtraFields = values.role === Role.ParentPatient;
 
@@ -175,7 +228,7 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
               }
               id="role"
               onBlur={handleBlur}
-              onChange={handleSelectOnChange}
+              onChange={handleSelectRoleOnChange}
               slotProps={{
                 popup: {
                   className: styles.popup,
@@ -251,13 +304,13 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
                 <label htmlFor="country">Country</label>
                 <Select
                   className={
-                    roleError
+                    countryError
                       ? [styles.select, styles['select--error']].join(' ')
                       : styles.select
                   }
                   id="country"
                   onBlur={handleBlur}
-                  onChange={handleSelectOnChange}
+                  onChange={handleSelectCountryOnChange}
                   slotProps={{
                     popup: {
                       className: styles.popup,
@@ -268,7 +321,7 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
                   }}
                   value={values.country}
                 >
-                  {roleOptions.map(({ label, value }) => (
+                  {englishContries.map(({ label, value }) => (
                     <Option
                       key={value}
                       value={value}
@@ -279,7 +332,88 @@ const SignupForm: React.FC<SignupFormProps> = ({ lang = 'en' }) => {
                     </Option>
                   ))}
                 </Select>
-                {roleError && <ErrorMessage errorMessage={errors.role} />}
+                {countryError && <ErrorMessage errorMessage={errors.country} />}
+              </div>
+            </div>
+          </div>
+          <div className={styles.form__row}>
+            <div className={styles.form__col}>
+              <div className={styles.form__field}>
+                <label htmlFor="region">
+                  State/Province/Region{' '}
+                  <span className="optional">(optional)</span>
+                </label>
+                <input
+                  id="region"
+                  name="region"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Region Name"
+                  type="text"
+                  value={values.region}
+                />
+                {regionError && <ErrorMessage errorMessage={errors.region} />}
+              </div>
+            </div>
+          </div>
+          <div className={styles.form__row}>
+            <div className={styles.form__col}>
+              <div className={styles.form__field}>
+                <label htmlFor="streetAdress">
+                  Street address <span className="optional">(optional)</span>
+                </label>
+                <input
+                  id="streetAdress"
+                  name="streetAdress"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="123 Street Name"
+                  type="text"
+                  value={values.streetAdress}
+                />
+                {streetAdressError && (
+                  <ErrorMessage errorMessage={errors.streetAdress} />
+                )}
+              </div>
+            </div>
+          </div>
+          <div className={styles.form__row}>
+            <div className={styles.form__col}>
+              <div className={styles.form__field}>
+                <label htmlFor="city">
+                  City <span className="optional">(optional)</span>
+                </label>
+                <input
+                  id="city"
+                  name="city"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="City Name"
+                  type="text"
+                  value={values.city}
+                />
+                {cityError && <ErrorMessage errorMessage={errors.city} />}
+              </div>
+            </div>
+          </div>
+          <div className={styles.form__row}>
+            <div className={styles.form__col}>
+              <div className={styles.form__field}>
+                <label htmlFor="postCode">
+                  Postal/zip code <span className="optional">(optional)</span>
+                </label>
+                <input
+                  id="postCode"
+                  name="postCode"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Postal/zip code"
+                  type="text"
+                  value={values.postCode}
+                />
+                {postCodeError && (
+                  <ErrorMessage errorMessage={errors.postCode} />
+                )}
               </div>
             </div>
           </div>
