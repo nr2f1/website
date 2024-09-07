10691e7f9d2207fb422551a72b07d2ce970f1a4f
Pedro Martin <pedro.martin@generalassemb.ly>
2024-09-07 14:14:58 +0100

    feat: signup success confirmation

--- website/src/services/givebutter/create-contact.ts
+++ website/src/services/givebutter/create-contact.ts
@@ -1,6 +1,13 @@
 import { Role, type SignupFormValues } from '@components/signup-form/helper';
 
 export const createContact = async (data: SignupFormValues) => {
+  const shouldAddAddress =
+    Boolean(data.country) &&
+    Boolean(data.region) &&
+    Boolean(data.city) &&
+    Boolean(data.streetAdress) &&
+    Boolean(data.postCode);
+
   const dto = {
     first_name: data.firstname,
     last_name: data.lastname,
@@ -12,9 +19,22 @@ export const createContact = async (data: SignupFormValues) => {
       custom_fields: {
         field_40498: data.patientFirstName,
       },
+    });
+  }
+
+  /*
+  Note:
+  Givebutter API does not support creating contacts with addresses when the country
+  and the zipcode regex do not match, but this is not documented in the API documentation
+  https://docs.givebutter.com/reference/create-contact
+  */
+
+  if (shouldAddAddress) {
+    Object.assign(dto, {
       addresses: [
         {
           address_1: data.streetAdress,
+          address_2: data.streetAdress,
           city: data.city,
           state: data.region,
           zipcode: data.postCode,
