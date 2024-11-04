1da18af33095891c2bdb9d315088dcc1ab3dd703
Pedro Martin <pataruco@gmail.com>
2024-11-04 13:32:33 +0000

    feat: scaffold homepage hero

--- website/src/app/[lang]/page.tsx
+++ website/src/app/[lang]/page.tsx
@@ -1,51 +1,11 @@
+import HomePageHero from '@components/homepage-hero';
 import type { PagePropsWithLocale } from '@shared/types/page-with-locale-params';
 import type { NextPage } from 'next';
 
 const Page: NextPage<PagePropsWithLocale> = async ({ params }) => {
   const { lang } = await params;
 
-  return (
-    <div className="content-wrapper">
-      <h1>Hola</h1>
-      <p>
-        Language is <strong>{lang}</strong>
-      </p>
-      <p>
-        <a href="/" className="button button--on-light">
-          button link
-        </a>
-      </p>
-      <button type="button" className="button" disabled>
-        button
-      </button>
-
-      <p>
-        <a href="/" className="button button--on-dark">
-          button link
-        </a>
-      </p>
-
-      <p>
-        <a href="/" className="button button--accent-on-light">
-          Donate
-        </a>
-      </p>
-      <button type="button" className="button button--accent-on-light">
-        Donate
-      </button>
-      <p>
-        <a href="/" className="button button--accent-on-dark">
-          button link
-        </a>
-      </p>
-      <button type="button" className="button button--accent-on-light" disabled>
-        Donate
-      </button>
-      <button type="button" className="button button--accent-on-dark" disabled>
-        Donate
-      </button>
-    </div>
-  );
+  return <HomePageHero lang={lang} />;
 };
 
 export default Page;
---
+++ website/src/components/homepage-hero/index.tsx
@@ -0,0 +1,63 @@
+import styles from './index.module.scss';
+
+import type { AvailableLocale } from '@i18n/locales';
+
+interface HomePageHeroProps {
+  lang: AvailableLocale;
+}
+
+const HomePageHero: React.FC<HomePageHeroProps> = ({ lang }) => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero__content_wrapper}>
+        <div className={styles.hero__row}>
+          <div className="col">
+            <section>
+              <h2>It's a rare diagnosis, but you're not alone</h2>
+              <p>
+                We're a non-profit organization that supports families and
+                individuals around the world living with rare NR2F1 gene
+                variants. These gene variants cause a neurodevelopmental
+                disorder called Bosch-Boonstra-Schaaf Optic Atrophy Syndrome
+                (BBSOAS).
+              </p>
+              <a href="/" className="button button--on-light">
+                Learn more about BBSOAS
+              </a>
+            </section>
+            <section>
+              <h3>How can we help you?</h3>
+              <nav>
+                <ul>
+                  <li>
+                    <a href="/" className="signpost-item">
+                      I’m a family member of a newly diagnosed patient
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/" className="signpost-item">
+                      I’m a scientific researcher
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/" className="signpost-item">
+                      I’m a medical professional
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </section>
+          </div>
+          <div className="col">
+            <img
+              src="https://pataruco.s3.amazonaws.com/public/hero-image.png"
+              alt="a woman pointing to the horizon with a girl sitting on her lap"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageHero;
