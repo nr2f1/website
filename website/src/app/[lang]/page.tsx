e37234540ca31fa98cdaebea8f6da53087e408a5
Pedro Martin <pataruco@gmail.com>
2024-11-06 15:21:41 +0000

    feat: markup waht we do

--- website/src/app/[lang]/page.tsx
+++ website/src/app/[lang]/page.tsx
@@ -1,11 +1,17 @@
 import HomePageHero from '@components/homepage-hero';
+import WhatWeDo from '@components/what-we-do';
 import type { PagePropsWithLocale } from '@shared/types/page-with-locale-params';
 import type { NextPage } from 'next';
 
 const Page: NextPage<PagePropsWithLocale> = async ({ params }) => {
   const { lang } = await params;
 
-  return <HomePageHero lang={lang} />;
+  return (
+    <>
+      <HomePageHero lang={lang} />
+      <WhatWeDo lang={lang} />
+    </>
+  );
 };
 
 export default Page;
