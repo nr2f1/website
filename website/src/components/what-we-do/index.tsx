32ca6e02f2ccc7bbc149bcf2ff5943dfaa5ff9ab
Pedro Martin <pataruco@gmail.com>
2024-11-07 11:23:15 +0000

    style: waht we do desktop

--- website/src/components/what-we-do/index.tsx
+++ website/src/components/what-we-do/index.tsx
@@ -17,33 +17,39 @@ const WhatWeDo: React.FC<WhatWeDoProps> = ({ lang }) => {
             is to empower families and individuals living with rare NR2F1
             variants through education, advocacy and research.
           </p>
-          <div className="row">
-            <div className="card">
+          <div className={styles.what_we_do__row}>
+            <div className={styles.card}>
               <div />
               <h3>Educate people</h3>
               <p>
                 Let us teach you about BBSOAS. We have recommended actions,
                 resources, and connections that can help.
               </p>
-              <a href="/">Learn more</a>
+              <a href="/" className="button button--on-light">
+                Learn more
+              </a>
             </div>
-            <div className="card">
+            <div className={styles.card}>
               <div />
               <h3>Empower families</h3>
               <p>
                 Our vision is that every single family and individual living
                 with BBSOAS will live a full and empowered life.
               </p>
-              <a href="/">Learn more</a>
+              <a href="/" className="button button--on-light">
+                Learn more
+              </a>
             </div>
-            <div className="card">
+            <div className={styles.card}>
               <div />
               <h3>Drive research</h3>
               <p>
                 So much about BBSOAS remains unknown. We work hard to uncover
                 the mysteries of this condition and develop therapies.
               </p>
-              <a href="/">Learn more</a>
+              <a href="/" className="button button--on-light">
+                Learn more
+              </a>
             </div>
           </div>
         </section>
