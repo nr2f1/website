62e1dbabb7a727d0259ef94a1b3ef07979dfcd84
Pedro Martin <pataruco@gmail.com>
2024-11-20 13:35:09 +0000

    feat: render post

--- website/src/app/[lang]/blog/[slug]/page.tsx
+++ website/src/app/[lang]/blog/[slug]/page.tsx
@@ -1,13 +1,18 @@
 import { getClient } from '@graphql/client';
 import {
+  GetPostDocument,
+  type GetPostQuery,
+} from '@graphql/queries/post/index.generated';
+import {
   GetPostsDocument,
   type GetPostsQuery,
 } from '@graphql/queries/posts/index.generated';
 import type { BlogPagePropsWithLocale } from '@shared/types/page-with-locale-params';
 import type { NextPage } from 'next';
 
+const { query } = getClient();
+
 export async function generateStaticParams() {
-  const { query } = getClient();
   const { data } = await query<GetPostsQuery>({
     query: GetPostsDocument,
   });
@@ -29,11 +34,36 @@ export async function generateStaticParams() {
 const Page: NextPage<BlogPagePropsWithLocale> = async ({ params }) => {
   const { lang, slug } = await params;
 
+  const { data } = await query<GetPostQuery>({
+    query: GetPostDocument,
+    variables: { locale: lang, slug },
+  });
+
+  if (
+    !data ||
+    !data.blogPageCollection ||
+    data.blogPageCollection.items.length === 0
+  ) {
+    return null;
+  }
+
+  const [post] = data.blogPageCollection.items;
+
+  console.log({ post });
+
+  const publishedDate = new Date(post?.date);
+
+  const publishedString = new Intl.DateTimeFormat(lang, {
+    dateStyle: 'long',
+  }).format(publishedDate);
+
   return (
-    <>
-      {/* <p>{lang}</p> */}
-      <p>hola</p>
-    </>
+    <div className="content-wrapper">
+      <section>
+        <h2>{post?.title}</h2>
+        <p>Published: {publishedString}</p>
+      </section>
+    </div>
   );
 };
 
