c5dde9ba1c2b960ee174d31a351a8f94b6e933ed
Pedro Martin <pataruco@gmail.com>
2024-11-23 09:36:27 +0000

    chore: latest news markup

---
+++ website/src/components/latest-news/index.tsx
@@ -0,0 +1,84 @@
+const LatestNews = () => {
+  return (
+    <section>
+      <div className="content-wrapper">
+        <h2>Latest News</h2>
+        <div className="articles">
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+          <article>
+            <picture>
+              <img src="https://picsum.photos/368/380" alt="" />
+            </picture>
+            <p>
+              <a href="/">Blog post</a>
+            </p>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+              <time dateTime="24/05/2024">May 24, 2024</time>
+            </p>
+          </article>
+        </div>
+        <a href="/">View all updates</a>
+      </div>
+    </section>
+  );
+};
