03cd0db81fa7192f7754ec16f2ac4ef981b56b93
Pedro Martin <pataruco@gmail.com>
2024-12-03 12:55:14 +0000

    chore: draft metadata

--- website/src/app/[lang]/page.tsx
+++ website/src/app/[lang]/page.tsx
@@ -3,9 +3,50 @@ import LatestNews from '@components/latest-news';
 import MembershipsPartners from '@components/memberships-partners';
 import SupportBanner from '@components/support-banner';
 import WhatWeDo from '@components/what-we-do';
+import { getClient } from '@graphql/client';
+import {
+  GetMetadataDocument,
+  type GetMetadataQuery,
+} from '@graphql/queries/metadata/index.generated';
+import { homepageMetadataId } from '@models/metadata';
 import type { PagePropsWithLocale } from '@shared/types/page-with-locale-params';
-import type { NextPage } from 'next';
+import type { Metadata, NextPage } from 'next';
 import HomePageBanner from './banner';
+
+export async function generateMetadata({
+  params,
+}: PagePropsWithLocale): Promise<Metadata> {
+  const { lang } = await params;
+
+  const { query } = getClient();
+  const { data, error } = await query<GetMetadataQuery>({
+    query: GetMetadataDocument,
+    variables: {
+      locale: lang,
+      id: homepageMetadataId,
+    },
+  });
+
+  if (
+    error ||
+    !data?.htmlHeadMetadata ||
+    !data?.htmlHeadMetadata?.title ||
+    !data?.htmlHeadMetadata?.description
+  ) {
+    return {
+      title: 'NR2F1 Foundation | Homepage',
+      description:
+        'These rare mutations cause a neurodevelopmental disorder called BBSOAS - Bosch-Boonstra-Schaaf optic atrophy syndrome',
+    };
+  }
+
+  const { title, description } = data.htmlHeadMetadata;
+
+  return {
+    title: `NR2F1 Foundation | ${title}`,
+    description,
+  };
+}
 
 const Page: NextPage<PagePropsWithLocale> = async ({ params }) => {
   const { lang } = await params;
