e91f3f4c576aa5ccb78de0cb922b055dd706e28b
Pedro Martin <pataruco@gmail.com>
2025-01-20 10:31:55 +0000

    feat: add metadata to register a patient page

--- website/src/app/[lang]/register-a-patient/page.tsx
+++ website/src/app/[lang]/register-a-patient/page.tsx
@@ -1,8 +1,16 @@
 import SupportBanner from '@components/support-banner';
+import { getClient } from '@graphql/client';
+import {
+  GetPageHeaderDocument,
+  type GetPageHeaderQuery,
+} from '@graphql/queries/page-header/index.generated';
+import { registerPatientPageHeaderId } from '@models/page-header';
 import type { PagePropsWithLocale } from '@shared/types/page-with-locale-params';
-import type { NextPage } from 'next';
+import type { Metadata, NextPage, ResolvingMetadata } from 'next';
 import RegisterPageBody from './page-body';
 import RegisterPageHeader from './page-header';
+
+const { query } = getClient();
 
 const Page: NextPage<PagePropsWithLocale> = async ({ params }) => {
   const { lang } = await params;
@@ -17,3 +25,39 @@ const Page: NextPage<PagePropsWithLocale> = async ({ params }) => {
 };
 
 export default Page;
+
+export async function generateMetadata(
+  { params }: PagePropsWithLocale,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const { lang } = await params;
+  const {
+    // @ts-ignore
+    title: { absolute },
+  } = await parent;
+
+  const {
+    data: {
+      // @ts-ignore
+      pageHeader: { title, sectionTitle },
+    },
+    error,
+  } = await query<GetPageHeaderQuery>({
+    query: GetPageHeaderDocument,
+    variables: {
+      locale: lang,
+      id: registerPatientPageHeaderId,
+    },
+  });
+
+  if (error || !title || !sectionTitle || !absolute) {
+    return {
+      title: 'NR2F1 FOUNDATION | Register a patient',
+    };
+  }
+
+  return {
+    title: `${absolute} | ${title}`,
+    description: sectionTitle,
+  };
+}
