9ebeecb364e64b08d814fdf0b5c5b8a59042044e
Pedro Martin <pataruco@gmail.com>
2025-01-24 17:23:02 +0000

    feat: chore localised 404 page

--- website/src/app/not-found.tsx
+++ website/src/app/not-found.tsx
@@ -10,6 +10,33 @@ import {
 import { headers } from 'next/headers';
 import Link from 'next/link';
 
+const translations: Record<AvailableLocale, Record<string, string>> = {
+  en: {
+    title: 'Sorry, this page isn’t available',
+    description:
+      'The link you followed may be broken, or the page may have been removed.',
+    cta: 'Back to homepage',
+  },
+  fr: {
+    title: 'Désolé, cette page n’est pas disponible',
+    description:
+      'Le lien que vous avez suivi peut être cassé ou la page a peut-être été supprimée.',
+    cta: 'Retour à la page d’accueil',
+  },
+  de: {
+    title: 'Entschuldigung, diese Seite ist nicht verfügbar',
+    description:
+      'Der Link, dem Sie gefolgt sind, ist möglicherweise defekt oder die Seite wurde möglicherweise entfernt.',
+    cta: 'Zurück zur Startseite',
+  },
+  es: {
+    title: 'Lo sentimos, esta página no está disponible',
+    description:
+      'El enlace que seguiste puede estar roto o es posible que se haya eliminado la página.',
+    cta: 'Volver a la página de inicio',
+  },
+};
+
 export default async function NotFound() {
   const headersList = await headers();
   const acceptLanguagesHeader = headersList.get('accept-language');
@@ -24,15 +51,14 @@ export default async function NotFound() {
     <div className={styles.notfound}>
       <div className={styles.notfound__content_wrapper}>
         <article>
-          <h1>Sorry, this page isn’t available</h1>
+          <h1>{translations[lang].title}</h1>
 
           <p>
-            <span>404 error</span>: The link you followed may be broken, or the
-            page may have been removed.
+            <span>404 error</span>: {translations[lang].description}
           </p>
 
           <Link href={`/${lang}`} className="button button--on-light">
-            Back to homepage
+            {translations[lang].cta}
           </Link>
         </article>
       </div>
