1ad156a8d2c2e2cc800f2ffe985da0e7ad7b3fd6
Pedro Martin <pataruco@gmail.com>
2025-02-10 15:43:38 +0000

    test: header UI elements

---
+++ website/e2e/header.test.ts
@@ -0,0 +1,87 @@
+import { expect, test } from '@playwright/test';
+import { english } from '../src/i18n/locales';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`/${english}`);
+});
+
+test('UI elements', async ({ page }) => {
+  await expect(
+    page.getByRole('link', { name: 'NR2F1 Foundation' }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('link', { name: 'Register a patient' }).first(),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('link', { name: 'Donate' }).first(),
+  ).toBeVisible();
+
+  await expect(page.getByText('About BBSOAS', { exact: true })).toBeVisible();
+
+  await expect(
+    page.getByText('Living with BBSOAS', { exact: true }),
+  ).toBeVisible();
+
+  await expect(page.getByText('Research', { exact: true })).toBeVisible();
+
+  await expect(page.getByText('About us')).toBeVisible();
+
+  await expect(page.getByRole('banner').getByText('Support us')).toBeVisible();
+});
+
+test('locale selector', async ({ page }) => {
+  await expect(
+    page
+      .locator('div')
+      .filter({
+        hasText:
+          /^English \(USA\)EspañolEnglish \(USA\)FrançaisDeutschRegister a patientDonate$/,
+      })
+      .getByRole('combobox'),
+  ).toBeVisible();
+
+  await page
+    .locator('div')
+    .filter({
+      hasText:
+        /^English \(USA\)EspañolEnglish \(USA\)FrançaisDeutschRegister a patientDonate$/,
+    })
+    .getByRole('combobox')
+    .click();
+
+  await page.getByRole('option', { name: 'Español' }).click();
+
+  await expect(
+    page
+      .locator('div')
+      .filter({
+        hasText:
+          /^EspañolEspañolEnglish \(USA\)FrançaisDeutschRegistrar un pacienteDonar$/,
+      })
+      .getByRole('combobox'),
+  ).toBeVisible();
+
+  await page
+    .locator('div')
+    .filter({
+      hasText:
+        /^EspañolEspañolEnglish \(USA\)FrançaisDeutschRegistrar un pacienteDonar$/,
+    })
+    .getByRole('combobox')
+    .click();
+  await page.getByRole('option', { name: 'English (USA)' }).click();
+
+  await expect(
+    page
+      .locator('div')
+      .filter({
+        hasText:
+          /^English \(USA\)EspañolEnglish \(USA\)FrançaisDeutschRegister a patientDonate$/,
+      })
+      .getByRole('combobox'),
+  ).toBeVisible();
+});
+
+// test('navigation', async ({ page }) => {});
