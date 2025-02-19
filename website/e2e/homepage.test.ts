279c17dcf6394231a50707599d9635e5bc16f92d
Pedro Martin <pataruco@gmail.com>
2025-02-19 20:29:52 +0000

    chore: create routes utils

--- website/e2e/homepage.test.ts
+++ website/e2e/homepage.test.ts
@@ -1,10 +1,11 @@
 import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 import { english } from '../src/i18n/locales';
+import { routes } from '../src/routes';
 import { a11yRules } from '../src/tests/a11y/config';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(`/${english}`);
+  await page.goto(routes.homepage(english));
 });
 
 test.describe('Homepage /en', () => {
