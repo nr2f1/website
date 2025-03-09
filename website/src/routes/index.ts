d6816024ca6e7db74b6d8d424665c11ccbc7173b
Pedro Martin <pataruco@gmail.com>
2025-03-09 13:41:25 +0000

    chore: update dependencies

--- website/src/routes/index.ts
+++ website/src/routes/index.ts
@@ -2,10 +2,22 @@ import type { AvailableLocale } from '@i18n/locales';
 
 type LocalisedRoute = (locale: AvailableLocale) => string;
 
-type RouteProperty = 'homepage';
+type RouteProperty =
+  | 'homepage'
+  | 'news'
+  | 'publications'
+  | 'register-a-patient';
 
 export const routes: Record<RouteProperty, LocalisedRoute> = {
   homepage(locale: AvailableLocale) {
     return `/${locale}`;
   },
+  news(locale: AvailableLocale) {
+    return `/${locale}/news`;
+  },
+  publications(locale: AvailableLocale) {
+    return `/${locale}/publications`;
+  },
+  'register-a-patient': (locale: AvailableLocale) =>
+    `/${locale}/register-a-patient`,
 };
