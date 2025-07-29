142a57c149ecd120db5affe0803cafe09240fc80
Pedro Martin <pataruco@gmail.com>
2025-07-29 12:12:12 +0100

    chore: update dependencies

---
+++ website/src/app/[lang]/privacy-policy/page-body.tsx
@@ -0,0 +1,14 @@
+import styles from './index.module.scss';
+
+
+export const PrivacyPolicyPageBody = () => {
+    return (
+    <div className={styles.privacypolicy}>
+               <div className={styles.privacypolicy__content_wrapper}>
+                 <article>
+
+                 </article>
+               </div>
+    </div>
+    );
+};
