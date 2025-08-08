2c524a2c7644dbfe9a767529e867e6aeb1b2d9ff
Pedro Martin <pataruco@gmail.com>
2025-08-08 15:57:25 +0100

    feat: store banner

---
+++ website/src/components/store-banner/index.tsx
@@ -0,0 +1,54 @@
+import Banner from '@components/banner';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { getClient } from '@graphql/client';
+import {
+  GetBannerDocument,
+  type GetBannerQuery,
+} from '@graphql/queries/banner/index.generated';
+import type { AvailableLocale } from '@i18n/locales';
+import { storeBannerId } from '@models/banners/banner';
+
+interface StoreBannerProps {
+  lang: AvailableLocale;
+}
+
+const { query } = getClient();
+
+const StoreBanner: React.FC<StoreBannerProps> = async ({ lang }) => {
+  const {
+    data: { banner },
+    error,
+  } = await query<GetBannerQuery>({
+    query: GetBannerDocument,
+    variables: {
+      id: storeBannerId,
+      locale: lang,
+    },
+  });
+
+  if (error || !banner) {
+    return null;
+  }
+
+  const { heading, content, cta, image } = banner;
+
+  const headingContent = heading?.content ?? '';
+  const textContent = documentToReactComponents(content?.content?.json);
+  const ctaContent = cta?.text?.content ?? '';
+  const ctaUrl = cta?.target?.url ?? '/';
+  const imageUrl = image?.url ?? '';
+
+  return (
+    <Banner
+      headingContent={headingContent}
+      textContent={textContent}
+      ctaContent={ctaContent}
+      ctaUrl={ctaUrl}
+      imageUrl={imageUrl}
+      theme="navy"
+      openNewTab
+    />
+  );
+};
+
+export default StoreBanner;
--- website/src/models/banners/banner.ts
+++ website/src/models/banners/banner.ts
@@ -3,3 +3,4 @@ export const supportUsBannerId = '1fsH8gLkk90N92o6Fv0ajd';
 export const researchBannerId = '1lqnXvKyIhDaCPVhZsReYK';
 export const volunteerWithUsBannerId = '3odHrsoLngYSYjQAd5HXhs';
 export const supportGroupBannerId = '16gd1oVcq9WpfSTw0AWVgF';
+export const storeBannerId = '6SuDgsaFkO3eCREf3UerB7';
