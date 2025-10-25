90a250b9d671598d95465140984052386067b1f3
Pedro Martin Valera <pataruco@gmail.com>
2025-10-25 19:01:13 +0100

    fix: hitting contentful rate limit

--- website/src/graphql/client.ts
+++ website/src/graphql/client.ts
@@ -1,4 +1,5 @@
-import { HttpLink } from '@apollo/client';
+import { createHttpLink, from, HttpLink } from '@apollo/client';
+import { RetryLink } from "@apollo/client/link/retry";
 import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev';
 import {
   ApolloClient,
@@ -8,6 +9,33 @@ import {
 import { Authorization, CONTENTUL_GRAPHQL_API } from '@config/utils';
 
 const isDev = process.env.NODE_ENV !== 'production';
+
+export const link = from([
+ 	new RetryLink({
+ 		attempts: (count, _operation, error) => {
+ 			const shouldRetry =
+ 				count < 5 &&
+ 				error.statusCode === 429 &&
+ 				error.response?.headers?.get('X-Contentful-RateLimit-Reset');
+
+ 			return shouldRetry;
+ 		},
+ 		delay: (count, _operation, error) => {
+ 			const delay =
+ 				Number(error.response?.headers?.get('X-Contentful-RateLimit-Reset')) *
+ 				1000;
+ 			console.info('CONTENTFUL-RATELIMIT: retry in ', delay, 'ms');
+ 			return delay;
+ 		},
+ 	}),
+ 	createHttpLink({
+ 		uri: CONTENTUL_GRAPHQL_API,
+ 		credentials: 'same-origin',
+ 		headers: {
+  			Authorization
+ 		},
+ 	}),
+ ]);
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
@@ -27,12 +55,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
         },
       },
     }),
-    link: new HttpLink({
-      headers: {
-        Authorization,
-      },
-      uri: CONTENTUL_GRAPHQL_API,
-    }),
+    link,
   });
 });
 
